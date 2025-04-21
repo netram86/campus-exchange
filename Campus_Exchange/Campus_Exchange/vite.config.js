f4dc690fb1d2de9db686386f80836507a5e30752
Netram <netramfaran831@gmail.com>
2025-04-21 17:56:14 +0530

    Initial commit

--- Campus_Exchange/Campus_Exchange/vite.config.js
+++ Campus_Exchange/Campus_Exchange/vite.config.js
@@ -3,22 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  preview: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-    rewrites: [
-      { source: '/(.*)', destination: '/index.html' }
-    ]
-  }
 })
